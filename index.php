f57b5797c376e29de04c47fd0c7fa1bb26aac120
Giuliano P. Tognarelli Buono-core <gtognarelli@entel.cl>
2018-06-26 17:05:26 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,81 @@
+<?php
+echo '';
+?>
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Somos la Unidad Digital de entel que entrega soluciones ágiles con toda la confianza de Entel">
+    <meta name="author" content="Entel Ocean">
+
+    <title>Entel Ocean · digital journey</title>
+
+    <link rel="icon" href="avatar-ocean.png">
+    <meta property="og:image" content="img/ocean.jpg" />
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/the-big-picture.css" rel="stylesheet">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121431948-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-121431948-1');
+    </script>
+
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-bottom">
+      <div class="container">
+        <a class="navbar-brand" href="#">Entel Ocean</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="https://www.linkedin.com/company/entelocean/" target="_blank">Linkedin
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://twitter.com/EntelOcean" target="_blank">Twitter</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://www.facebook.com/entelocean/" target="_blank">Facebook</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://www.instagram.com/entelocean/" target="_blank">Instagram</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Content -->
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <h1 class="mt-5" style="max-width: 400px;"><img src="img/ocean.svg" class="mt-5"></h1>
+            <p style="color:white;">Nuestros consumidores y dispositivos generan un mar de datos que a través de nuevas tecnologías podremos navegar, explorar y entender.
+              <br>Un nuevo océano posible de conquistar <span style="color:#f16a45;">(</span>...<span style="color:#f16a45;">)</span></p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
